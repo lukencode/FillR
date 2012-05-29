11cf1d0b002536096ef310ce0dc3f7a73082f6b3
Luke Lowrey <lukenlowrey@gmail.com>
2012-05-29 16:25:36 +1000

    added project

---
+++ FillR/FillR.cs
@@ -0,0 +1,16 @@
+﻿
+namespace FillR
+{
+    public static class FillR
+    {
+        public static T Fill<T>(this T item)
+        {
+            return item;
+        }
+
+        public static T Fill<T>()
+        {
+            return default(T);
+        }
+    }
+}
